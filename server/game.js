c43ce7ca68b269478dc29116bea55f2930ed1bd2
Eric <Ericrius1@gmail.com>
2013-06-05 15:31:05 -0700

    checking for players in lobby

--- server/game.js
+++ server/game.js
@@ -1,13 +1,35 @@
 ////////// Server only logic //////////
 
+var numPlayersLooking;
+var numPlayersPerGame;
 
 Meteor.startup(function() {
+  numPlayersLooking=0;
+  numPlayersPerGame = 2;
   var query = Players.find({
     looking: true
   });
   var handle = query.observeChanges({
     added: function() {
-      console.log("player added!")
+      numPlayersLooking++;
+      console.log(numPlayersLooking);
+      if(numPlayersLooking == numPlayersPerGame){
+        Meteor.call("start_new_game");
+      }
     }
   });
+});
+
+Meteor.methods({
+
+  start_new_game: function(){
+    console.log("new game started")
+    //create a new game
+    var game_id = Games.insert({});
+
+    //Move everyone who declared themselves ready in the lobby into the game
+    Players.update({game_id: null, looking:true},
+      {$set: {game_id: game_id}},
+      {multi: true});
+  }
 });
