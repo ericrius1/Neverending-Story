5d5b0b2bcd66bf7ceaaff6efb64543790735d03c
Eric <Ericrius1@gmail.com>
2013-06-06 13:31:34 -0700

    player must enter their name

--- client/lobby/lobby.js
+++ client/lobby/lobby.js
@@ -1,27 +1,43 @@
-
-Template.lobby.show_lobby = function () {
+Template.lobby.show_lobby = function() {
   // only show lobby if we're not in a game
   return !Meteor.clientMethods.game();
 };
 
 Template.lobby.looking = function() {
-  var player = Players.findOne({_id: Session.get('player_id')});
-  if(player!== undefined){
+  var player = Players.findOne({
+    _id: Session.get('player_id')
+  });
+  if (player !== undefined) {
     return player.looking;
   }
 };
 
 Template.lobby.remaining_players = function() {
- return Meteor.globalMethods.remaining_players();
+  return Meteor.globalMethods.remaining_players();
+};
+
+Template.lobby.disabled = function() {
+  var me = Meteor.clientMethods.player();
+  if (me && me.name)
+    return '';
+  return 'disabled="disabled"';
 };
 
 Template.lobby.events({
-  'keyup input#myname': function (evt) {
+  'keyup input#myname': function(evt) {
     var name = $('#lobby input#myname').val().trim();
-    Players.update(Session.get('player_id'), {$set: {name: name}});
+    Players.update(Session.get('player_id'), {
+      $set: {
+        name: name
+      }
+    });
   },
-  'click button.join_story': function () {
-    Players.update(Session.get('player_id'), {$set: {looking: true}})
+  'click button.join_story': function() {
+    Players.update(Session.get('player_id'), {
+      $set: {
+        looking: true
+      }
+    })
   }
 });
 
@@ -29,17 +45,19 @@ Template.lobby.events({
 ////// Initialization
 //////
 
-Meteor.startup(function () {
+Meteor.startup(function() {
   // Allocate a new player id.
   //
   // XXX this does not handle hot reload. In the reload case,
   // Session.get('player_id') will return a real id. We should check for
   // a pre-existing player, and if it exists, make sure the server still
   // knows about us.
-  var player_id = Players.insert({name: ''});
+  var player_id = Players.insert({
+    name: ''
+  });
   Session.set('player_id', player_id);
 
-  Deps.autorun(function () {
+  Deps.autorun(function() {
     Meteor.subscribe('players');
 
     if (Session.get('player_id')) {
