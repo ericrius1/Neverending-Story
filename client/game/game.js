2e129f00b07b61a2c57402e694fa346a61ef5799
Eric <Ericrius1@gmail.com>
2013-06-06 11:34:42 -0700

    when game starts, players submission boxes are appearing on screen

--- client/game/game.js
+++ client/game/game.js
@@ -2,3 +2,6 @@ Template.game.show_game = function() {
   return Meteor.clientMethods.game(); 
 }
 
+Template.game.players = function() {
+  return Players.find({game_id: Meteor.clientMethods.game()});
+}
