635e234d2773168eefe70a9fca7d77ffd5e9972b
Eric <Ericrius1@gmail.com>
2013-06-07 16:20:28 -0700

    text disapearing after clock runs down

--- client/game/game.js
+++ client/game/game.js
@@ -3,40 +3,11 @@ Template.game.show = function() {
   return client.game()!==undefined; 
 }
 
+Template.game.my_name = function(){
+  return client.player().name;
+}
+
 
 Template.clock.clock = function() {
   return client.timer().clock
-}
-
-Template.game.other_players = function() {
-  return client.other_players();
-}
-
-Template.player.voting = function(){
-  return client.is_voting();
-}
-
-
-Template.other_player.voting = function(){
-  return client.is_voting();
-}
-
-Template.other_player.events({
-  'click button': function(evt) {
-    if(evt.type === "click"){
-      var input_id = "#submission_" + this.name;
-      var text = $(input_id).text();
-    }
-  }
-})
-
-
-Template.other_player.events()
-
-Template.player.player = function(){
-  return client.player();
-}
-
-Template.player.my_name = function(){
-  return client.player().name;
 }
