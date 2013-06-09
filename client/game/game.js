cebf023924bb58c2c49288226c3af146ba01a83e
Eric <Ericrius1@gmail.com>
2013-06-09 14:46:29 -0700

    got rid of bug where focus wasn't returning to player

--- client/game/game.js
+++ client/game/game.js
@@ -2,8 +2,8 @@ Template.game.show = function() {
   return client.game()!==undefined; 
 }
 
-Template.game.track_round = function() {
-  client.track_round();
+Template.game.setup_round = function() {
+  client.setup_round();
 }
 
 Template.story.content = function(){
@@ -28,7 +28,6 @@ Template.game.submission = function(){
   return client.selected_player_submission(selectedPlayerId);
 }
 
-
 Template.game.selected= function(){
   //returns selected if the current tab is our active tab otherwise our own tab is active
   if(!client.is_voting()){
@@ -39,7 +38,6 @@ Template.game.selected= function(){
 
 Template.game.readonly = function(){
   //returns readonly if we are in voting round or selected tab is not our own
-  debugger;
   return client.is_voting() || Session.get('game.activeTab') !== client.player()._id ? "readonly" : "";
 }
 
@@ -62,6 +60,7 @@ Template.game.getActiveTab = function() {
 
 Template.game.events({
   'click nav.tabs > a': function(evt) {
+    if(!client.is_voting())return;
     var id = $(evt.target).data('playerId');
     Session.set('game.activeTab', id);
     $(evt.target).attr('selected', 'selected');
