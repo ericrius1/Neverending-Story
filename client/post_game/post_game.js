0670cb488b7cbd732ea7b58b8106c53966b86dd5
Eric <Ericrius1@gmail.com>
2013-06-10 09:23:22 -0700

    updated

--- client/post_game/post_game.js
+++ client/post_game/post_game.js
@@ -1,3 +1,20 @@
-Template.post_game.show = function(){
+Template.post_game.show = function() {
   return client.game() && client.game().gameOver;
-}
+};
+
+// Template.welcomePage.rendered = function () {
+//     addTwitterWidget();
+// };
+ 
+// function addTwitterWidget() {
+//     !function (d, s, id) {
+//         var js, fjs = d.getElementsByTagName(s)[0], 
+//             p = /^http:/.test(d.location) ? 'http' : 'https';
+//         if (!d.getElementById(id)) {
+//             js = d.createElement(s);
+//             js.id = id;
+//             js.src = p + '://platform.twitter.com/widgets.js';
+//             fjs.parentNode.insertBefore(js, fjs);
+//         }
+//     }(document, 'script', 'twitter-wjs');
+// };
